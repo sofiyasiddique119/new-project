78f32744b4355beef158a6b4229d59eaebcc10bf
Sofiya Siddique <sofiyasiddique119@gmail.com>
2024-12-10 15:31:59 +0530

    changes

---
+++ main.js
@@ -0,0 +1,8 @@
+// var navLinks = document.getElementById("navLinks");
+console.log("hello")
+function showMenu() {
+    navLinks.style.right="0";
+}
+function hideMenu() {
+    navLinks.style.right="-200px";
+}
